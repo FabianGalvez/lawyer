76291154cfcbf3b84baed51c6d32c90da8cb0689
Fabián Gálvez <fabian_galvez@hotmail.com>
2016-04-12 23:46:32 -0500

    tercero

--- app_client/common/services/authentication.service.js
+++ app_client/common/services/authentication.service.js
@@ -12,60 +12,53 @@
       return $window.localStorage['lawyer-token'];
     };
 
+    var register = function(user) {
+      return $http.post('/api/register', user).success(function(data){
+        saveToken(data.token);
+      });
+    };
+
+    var login = function(user) {
+      return $http.post('/api/login', user).success(function(data) {
+        saveToken(data.token);
+      });
+    };
+
+    var logout = function() {
+      $window.localStorage.removeItem('lawyer-token');
+    };
+
+    var isLoggedIn = function() {
+      var token = getToken();
+
+      if(token){
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return payload.exp > Date.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+
+    var currentUser = function() {
+      if(isLoggedIn()){
+        var token = getToken();
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        return {
+          email : payload.email,
+          name : payload.name
+        };
+      }
+    };
+
+
     return {
       saveToken : saveToken,
-      getToken : getToken
+      getToken : getToken,
+      register : register,
+      login : login,
+      logout : logout,
+      isLoggedIn : isLoggedIn,
+      currentUser : currentUser
     };
   }
 })();
-
-register = function(user) {
-  return $http.post('/api/register', user).success(function(data){
-    saveToken(data.token);
-  });
-};
-
-
-login = function(user) {
-  return $http.post('/api/login', user).success(function(data) {
-    saveToken(data.token);
-  });
-};
-
-logout = function() {
-  $window.localStorage.removeItem('lawyer-token');
-};
-
-var isLoggedIn = function() {
-  var token = getToken();
-
-  if(token){
-    var payload = JSON.parse($window.atob(token.split('.')[1]));
-    return payload.exp > Date.now() / 1000;
-  } else {
-    return false;
-  }
-};
-
-var currentUser = function() {
-  if(isLoggedIn()){
-    var token = getToken();
-    var payload = JSON.parse($window.atob(token.split('.')[1]));
-    return {
-      email : payload.email,
-      name : payload.name
-    };
-  }
-};
-
-function navigationCtrl($location, authentication) {
-  var vm = this;
-
-  vm.currentPath = $location.path();
-  vm.isLoggedIn = authentication.isLoggedIn();
-  vm.currentUser = authentication.currentUser();
-  vm.logout = function() {
-    authentication.logout();
-    $location.path('/');
-  };
-}
